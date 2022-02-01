aafecf5255cabe29d3c04535515c7d13f0a794f6
Adrian <adrian.jazzdorf@gmail.com>
2022-02-01 14:04:43 +0000

    added function compute() functionality for formatting result text

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function compute()
     // add amount to principal
     let amount = interest + parseInt(principal);
     var year = new Date().getFullYear()+parseInt(years);
-    var interestString = "If you deposit " + principal.bold() + ",\n at an interest rate of " + rate + ".\n You will receive an amount of " + amount + ",\n in the year " + year + ".";
-    document.getElementById("result").innerText=interestString
+    var interestString = "If you deposit " + principal.bold() + ",\<br/>at an interest rate of " + rate.bold() + ".\<br/>You will receive an amount of " + String(amount).bold() + ",\<br/>in the year " + String(year).bold() + ".";
+    document.getElementById("result").innerHTML=interestString
 
 }
         
