c9af0e6ee735251fb2b6d1e18076e68fe09b3d57
Adrian <adrian.jazzdorf@gmail.com>
2022-02-01 15:28:36 +0000

    changed the calculation method

--- script.js
+++ script.js
@@ -18,10 +18,8 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = Math.round(parseInt(principal) * (rate / 100) * years * 100)/100;
-    // add amount to principal
-    let amount = interest + parseInt(principal);
     var year = new Date().getFullYear()+parseInt(years);
-    var interestString = "If you deposit " + principal.bold() + ",\<br/>at an interest rate of " + rate.bold() + ".\<br/>You will receive an amount of " + String(amount).bold() + ",\<br/>in the year " + String(year).bold() + ".";
+    var interestString = "If you deposit " + principal.bold() + ",\<br/>at an interest rate of " + rate.bold() + ".\<br/>You will receive an amount of " + String(interest).bold() + ",\<br/>in the year " + String(year).bold() + ".";
     document.getElementById("result").innerHTML=interestString
 
 }
