5c9465b7435540792eb5de0db18921dff9ca4c90
Adrian <adrian.jazzdorf@gmail.com>
2022-02-01 15:06:08 +0000

    added validation functionality

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+
+    if(principal == ""){
+        alert("Please enter principal.");
+        principal.focus();
+        return false;
+    }
+
+    if(principal <= 0){
+        alert("Please enter a principal greater than 0.")
+        principal.focus();
+        return false;
+    }
+
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * (rate / 100) * years;
+    var interest = Math.round(parseInt(principal) * (rate / 100) * years * 100)/100;
     // add amount to principal
     let amount = interest + parseInt(principal);
     var year = new Date().getFullYear()+parseInt(years);
