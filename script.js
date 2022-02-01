86367927aeb0f560459afcce4bfff117196c679e
Adrian <adrian.jazzdorf@gmail.com>
2022-02-01 13:54:30 +0000

    changed compute function

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 100;
+    var interest = principal * (rate / 100) * years;
+    // add amount to principal
+    let amount = interest + parseInt(principal);
     var year = new Date().getFullYear()+parseInt(years);
-    alert("button has been clicked");
-    document.getElementById("result").innerText="If you depost " + principal;
+    var interestString = "If you deposit " + principal.bold() + ",\n at an interest rate of " + rate + ".\n You will receive an amount of " + amount + ",\n in the year " + year + ".";
+    document.getElementById("result").innerText=interestString
 
 }
         
